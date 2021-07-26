4fa9d416364d557616cb0a0455969638e127d58c
JuSoft <70066080+jusoftdev@users.noreply.github.com>
2021-07-26 08:14:52 +0200

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 // API key https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'insert your own key lmao'
+export const API_KEY = 'AIzaSyDmvqc-dtVQCIMlCOGNtSpJeIzF-rX5LHM' //Demo key - for localhost please insert your own key 
 
 // Context key https://cse.google.com/cse/create/new
 export const CONTEXT_KEY = '9867c733a42c0c643'
