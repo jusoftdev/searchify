019c79d46e3f7aca0e85f733ea8b576ec0edec68
juhom205 <80626044+juhom205@users.noreply.github.com>
2022-04-09 18:03:36 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <footer className='grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-400 dark:bg-gray-800'>
             <div className="px-8 py-3">
-                <p>Beta 1.0.1</p>
+                <p>Beta 1.0.2</p>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3'>
                 <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
@@ -17,8 +17,6 @@ function Footer(){
                     <p>Discord</p>
                 </div>
                 <div className="flex justify-center space-x-8 md:ml-auto">
-                    <p>Privacy</p>
-                    <a href="https://jusoft.dev/impressum.html"><p className="link">Legal Notice</p></a>
                     <p>Settings</p>
                 </div>
             </div>
